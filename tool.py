beeeeebb740839cc175d6eb9a18a872e4cb7d38e
Samuel Ambros <xambros@fi.muni.cz>
2024-04-18 21:53:42 +0200

    finish datasets

--- tool.py
+++ tool.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from shared.helpers import load_model
@@ -8,6 +8,7 @@ import io
 import requests
 from scipy.ndimage import shift
 import numpy as np
+
 # for KTR model this import is needed
 from orbit.models import KTR
 from sklearn.preprocessing import StandardScaler
@@ -15,21 +16,41 @@ from shared.lstm import create_data
 from shared.transformer import TransformerModel
 from shared.gnn import GNNModel
 
+dataset_metadata = {
+    "covid_deaths": {
+        "date_column": "Date_reported",
+        "target_column": "New_deaths",
+        "id_column": "WHO_region",
+        "sequence_length": 14,
+        "extra_columns": ["New_cases_30_days_ago"],
+    },
+    "btc": {
+        "date_column": "Date",
+        "target_column": "Close",
+        "id_column": None,
+        "sequence_length": 14,
+        "extra_columns": ["Closed_30_days_ago"],
+    },
+}
+
+
 def load_covid_deaths():
-    target_column = "New_deaths"
     url = "https://covid19.who.int/WHO-COVID-19-global-data.csv"
-    date_column = "Date_reported"
     s = requests.get(url).content
     df = pd.read_csv(io.StringIO(s.decode("utf-8")))
-    df = df[df[target_column] >= 0]
+    df = df[df[dataset_metadata["covid_deaths"]["target_column"]] >= 0]
     df = df[df["New_cases"] >= 0]
-    df_max = df[target_column].max()
-    df = df[df[target_column] != df_max]
-    df[target_column] = df[target_column] + 1
-    df[date_column] = pd.to_datetime(df[date_column])
+    df_max = df[dataset_metadata["covid_deaths"]["target_column"]].max()
+    df = df[df[dataset_metadata["covid_deaths"]["target_column"]] != df_max]
+    df[dataset_metadata["covid_deaths"]["target_column"]] = (
+        df[dataset_metadata["covid_deaths"]["target_column"]] + 1
+    )
+    df[dataset_metadata["covid_deaths"]["date_column"]] = pd.to_datetime(
+        df[dataset_metadata["covid_deaths"]["date_column"]]
+    )
     all_dates = pd.date_range(
         start=df["Date_reported"].min(),
-        end=df["Date_reported"].max() + datetime.timedelta(30),
+        end=df["Date_reported"].max() + timedelta(30),
         freq="D",
     )
     groups = []
@@ -54,17 +75,19 @@ def load_covid_deaths():
 
 
 def load_btc():
-    date_column = "Date"
-    target_column = "Close"
     now = int(datetime.now(tz=pytz.utc).timestamp())
     url = f"https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1681404460&period2={now}&interval=1d&events=history"
     print(url)
-    df = pd.read_csv(url, parse_dates=True)[[date_column, target_column]]
+    df = pd.read_csv(url, parse_dates=True)[
+        [
+            dataset_metadata["btc"]["date_column"],
+            dataset_metadata["btc"]["target_column"],
+        ]
+    ]
     new_df = df.copy().iloc[30:]
-    new_column = shift(df[target_column], shift=30)[30:]
+    new_column = shift(df[dataset_metadata["btc"]["target_column"]], shift=30)[30:]
     new_df[f"Closed_30_days_ago"] = new_column
     df = new_df
-    df.set_index(date_column, inplace=True, drop=False)
     return df
 
 
@@ -78,73 +101,132 @@ def load_dataset(dataset_name):
         raise ValueError("Invalid dataset name")
 
 
-def predict_arima(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    df = df[[date_column, target_column, "New_cases_30_days_ago"]]
-    df = df.groupby(date_column).sum().reset_index()
-    df[date_column] = pd.to_datetime(df.pop(date_column), format="%Y-%m-%d")
+def predict_arima(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            dataset_metadata[dataset_name]["target_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df.pop(dataset_metadata[dataset_name]["date_column"]), format="%Y-%m-%d"
+    )
     return (
-        model.predict(start=df[date_column].values[-30], end=df[date_column].values[-1])
+        model.predict(
+            start=df[dataset_metadata[dataset_name]["date_column"]].values[-30],
+            end=df[dataset_metadata[dataset_name]["date_column"]].values[-1],
+        )
         - 1
     )
 
 
-def predict_regression(model, df):
-    date_column = "Date_reported"
-    df = df[[date_column, "New_cases_30_days_ago"]]
-    df = df.groupby(date_column).sum().reset_index()
-    df.set_index(date_column, inplace=True)
+def predict_regression(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df.set_index(dataset_metadata[dataset_name]["date_column"], inplace=True)
     df = df.select_dtypes(exclude=["object"])
     return model.predict(df.iloc[-30:]) - 1
 
 
-def predict_bayes(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    df = df[[date_column, target_column, "New_cases_30_days_ago"]]
-    df = df.groupby(date_column).sum().reset_index()
-    df[date_column] = pd.to_datetime(df.pop(date_column), format="%Y-%m-%d")
+def predict_bayes(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            dataset_metadata[dataset_name]["target_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df.pop(dataset_metadata[dataset_name]["date_column"]), format="%Y-%m-%d"
+    )
     return model.predict(df.iloc[-30:])["prediction"] - 1
 
-def predict_prohpeth(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    df = df[[date_column, target_column, "New_cases_30_days_ago"]]
-    df = df.groupby(date_column).sum().reset_index()
-    df[date_column] = pd.to_datetime(df.pop(date_column), format="%Y-%m-%d")
-    df = df.rename(columns={date_column: "ds", target_column: "y"})
-    return model.predict(df.iloc[-30:][['ds']])["yhat"] - 1
 
-def predict_prohpeth_log(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    df = df[[date_column, target_column, "New_cases_30_days_ago"]]
-    df = df.groupby(date_column).sum().reset_index()
-    df[date_column] = pd.to_datetime(df.pop(date_column), format="%Y-%m-%d")
-    df = df.rename(columns={date_column: "ds", target_column: "y"})
-    return np.exp(model.predict(df.iloc[-30:][['ds']])["yhat"]) - 1
-
-def predict_automl(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    df = df[[date_column, target_column, 'New_cases_30_days_ago']]
-    df[date_column] = pd.to_datetime(df[date_column], format="%Y-%m-%d")
+def predict_prohpeth(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            dataset_metadata[dataset_name]["target_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df.pop(dataset_metadata[dataset_name]["date_column"]), format="%Y-%m-%d"
+    )
+    df = df.rename(
+        columns={
+            dataset_metadata[dataset_name]["date_column"]: "ds",
+            dataset_metadata[dataset_name]["target_column"]: "y",
+        }
+    )
+    return model.predict(df.iloc[-30:][["ds"]])["yhat"] - 1
 
 
-    df = df.groupby(date_column).sum().reset_index()
-    df.set_index(date_column, inplace=True)
+def predict_prohpeth_log(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            dataset_metadata[dataset_name]["target_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df.pop(dataset_metadata[dataset_name]["date_column"]), format="%Y-%m-%d"
+    )
+    df = df.rename(
+        columns={
+            dataset_metadata[dataset_name]["date_column"]: "ds",
+            dataset_metadata[dataset_name]["target_column"]: "y",
+        }
+    )
+    return np.exp(model.predict(df.iloc[-30:][["ds"]])["yhat"]) - 1
+
+
+def predict_automl(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            dataset_metadata[dataset_name]["target_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df[dataset_metadata[dataset_name]["date_column"]], format="%Y-%m-%d"
+    )
+
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df.set_index(dataset_metadata[dataset_name]["date_column"], inplace=True)
     print(df)
     model.fit_data(df.iloc[:-30])
-    return (model.predict(forecast_length=1).forecast[target_column] - 1)[:30]
+    return (
+        model.predict(forecast_length=1).forecast[
+            dataset_metadata[dataset_name]["target_column"]
+        ]
+        - 1
+    )[:30]
 
-def predict_lstm(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    df = df[[date_column, target_column, "New_cases_30_days_ago"]]
 
-    df = df.groupby(date_column).sum().reset_index()
-    df.set_index(date_column, inplace=True)
+def predict_lstm(model, df, dataset_name):
+    df = df[
+        [
+            dataset_metadata[dataset_name]["date_column"],
+            dataset_metadata[dataset_name]["target_column"],
+            *dataset_metadata[dataset_name]["extra_columns"],
+        ]
+    ]
+
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
+    df.set_index(dataset_metadata[dataset_name]["date_column"], inplace=True)
     df = df.dropna()
     scaler = StandardScaler()
     scaled_data = scaler.fit_transform(df)
@@ -158,52 +240,63 @@ def predict_lstm(model, df):
     )
     y_pred = model.predict(X_test)
     scaler = StandardScaler()
-    scaler.fit(df[[target_column]])
+    scaler.fit(df[[dataset_metadata[dataset_name]["target_column"]]])
     unscaled_y_pred = scaler.inverse_transform([y_pred.flatten()])[0]
     return unscaled_y_pred - 1
 
-def predict_transformer(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
 
-    df = df.groupby(date_column).sum().reset_index()
+def predict_transformer(model, df, dataset_name):
+    df = df.groupby(dataset_metadata[dataset_name]["date_column"]).sum().reset_index()
     df = df.dropna()
 
-    df["id"] = "id"
-    df[date_column] = pd.to_datetime(df[date_column])
-    df[target_column] = df[target_column].fillna(0)
-    df.set_index(date_column, inplace=True)
+    df["item_id"] = "id"
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df[dataset_metadata[dataset_name]["date_column"]]
+    )
+    df[dataset_metadata[dataset_name]["target_column"]] = df[
+        dataset_metadata[dataset_name]["target_column"]
+    ].fillna(0)
+    df.set_index(dataset_metadata[dataset_name]["date_column"], inplace=True)
     return model.predict(df)
 
-def predict_gnn(model, df):
-    date_column = "Date_reported"
-    target_column = "New_deaths"
-    id_column = "WHO_region"
-    df[date_column] = pd.to_datetime(df[date_column])
-    df = df.groupby([date_column, id_column])[target_column].sum().reset_index()
+
+def predict_gnn(model, df, dataset_name):
+    df[dataset_metadata[dataset_name]["date_column"]] = pd.to_datetime(
+        df[dataset_metadata[dataset_name]["date_column"]]
+    )
+    df = (
+        df.groupby(
+            [
+                dataset_metadata[dataset_name]["date_column"],
+                dataset_metadata[dataset_name]["id_column"],
+            ]
+        )[dataset_metadata[dataset_name]["target_column"]]
+        .sum()
+        .reset_index()
+    )
     return model.predict(df)
 
-def predict(model, df, forecast_length, model_name):
-    if model_name == "arima":
-        return predict_arima(model, df)
-    elif model_name == "sarima":
-        return predict_arima(model, df)
+
+def predict(model, df, model_name, dataset_name):
+    if model_name == "arima" or model_name == "sarima":
+        return predict_arima(model, df, dataset_name)
     elif model_name == "regression":
-        return predict_regression(model, df)
+        return predict_regression(model, df, dataset_name)
     elif model_name == "ets" or model_name == "ktr" or model_name == "dlt":
-        return predict_bayes(model, df)
+        return predict_bayes(model, df, dataset_name)
     elif model_name == "prophet":
-        return predict_prohpeth(model, df)
+        return predict_prohpeth(model, df, dataset_name)
     elif model_name == "prophet_log":
-        return predict_prohpeth_log(model, df)
+        return predict_prohpeth_log(model, df, dataset_name)
     elif model_name == "automl":
-        return predict_automl(model, df)
+        return predict_automl(model, df, dataset_name)
     elif model_name == "lstm":
-        return predict_lstm(model, df)
+        return predict_lstm(model, df, dataset_name)
     elif model_name == "transformer":
-        return predict_transformer(model, df)
+        return predict_transformer(model, df, dataset_name)
     elif model_name == "gnn":
-        return predict_gnn(model, df)
+        return predict_gnn(model, df, dataset_name)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -224,7 +317,7 @@ def main():
             "ets",
             "ktr",
             "automl",
-            "gnn"
+            "gnn",
         ],
     )
     parser.add_argument(
@@ -243,9 +336,7 @@ def main():
         default=30,
     )
     args = parser.parse_args()
-    model_name = "gnn"
-    # model_name = args.model
-    args.dataset = "covid_deaths"
+    model_name = args.model
     if args.forecastLength > 30:
         raise ValueError("The forecast length cannot exceed 30")
     if not model_name:
@@ -255,6 +346,8 @@ def main():
             model_name = "lstm"
     dataset = load_dataset(args.dataset)
     if model_name == "gnn":
+        if args.dataset == "btc":
+            raise ValueError("GNN model is not supported for this dataset")
         model = GNNModel(
             date_column="Date_reported",
             target_column="New_deaths",
@@ -263,7 +356,7 @@ def main():
         )
     else:
         model = load_model(model_name, args.dataset)
-    predictions = predict(model, dataset, args.forecastLength, model_name)
+    predictions = predict(model, dataset, model_name, args.dataset)
     for i in predictions:
         print(i)
 
